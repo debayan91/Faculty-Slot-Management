6155f61f4006361b091d871bf1a2f23164884063
Debayan Dutta <debayan04dutta@gmail.com>
2025-11-01 19:57:09 +0000

    the borders are still black not grey consistent w everything

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -141,7 +141,7 @@ export default function LoginPage() {
               disabled={isLoading}
               type="button"
             >
-              <svg className="google-icon" viewBox="0 0 48 48">
+              <svg className="mr-3 h-5 w-5" viewBox="0 0 48 48">
                 <path
                   fill="#EA4335"
                   d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
