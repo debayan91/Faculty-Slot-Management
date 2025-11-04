11d4e11cbcc1b2b68da6f4a12886ddf353d7dfa4
Debayan Dutta <debayan04dutta@gmail.com>
2025-11-04 15:23:11 +0000

    go back to what it was

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -19,12 +19,9 @@ export function ThemeToggle() {
       size="icon"
       onClick={toggleTheme}
       aria-label="Toggle theme"
-      className="group"
     >
-      <div className="relative h-6 w-6 transition-transform duration-500 group-hover:rotate-90">
-        <Sun className="absolute inset-0 h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute inset-0 h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      </div>
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
