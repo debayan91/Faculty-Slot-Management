ffefe86e4ecdd0856f46d0f8f2170f479f17489c
Debayan Dutta <debayan04dutta@gmail.com>
2025-11-04 15:18:20 +0000

    including the search one

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -208,7 +208,7 @@ export function Header() {
             </div>
             <div className="relative">
                 <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-                <Input placeholder="Search..." className="pl-9 h-8 w-48 md:w-64" />
+                <Input placeholder="Search..." className="pl-9 h-8 w-48 md:w-64 no-shadow" />
             </div>
         </div>
       </nav>
